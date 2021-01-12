91f8ab334b23a2e07ae09a04c0e6f54f430c1ecf
oluwadara <daraoloye99@gmail.com>
2021-01-12 13:10:05 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express();
+const port = process.env.PORT || 3003
+const router = express.Router()
+
+app.use(express.static(`${__dirname}/dist`)) 
+
+// set the static files location for the static html
+app.engine('.html', require('ejs').renderFile)
+
+app.set('views', `${__dirname}/dist`)
+
+// eslint-disable-next-line no-unused-vars
+router.get('/*', (req, res, next) => { 
+    res.sendFile(`${__dirname}/dist/index.html`)
+})
+
+app.use('/', router)
+
+app.listen(port)
+
+console.log('App running on port', port)
