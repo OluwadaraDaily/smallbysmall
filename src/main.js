2c4c0c3e43e6b322bc4c727a4617fc88defabf48
oluwadara <daraoloye99@gmail.com>
2021-01-13 14:29:45 +0100

    Added JS Integration for Autocapture-PostHog

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Posthog from 'posthog-js'
 
 Vue.config.productionTip = false
+
+Posthog.init("[iHGijN_zMe0ULDD9khFpPOcOfUGpwxl4nokVNsMvHEY]", {api_host: 'http://159.203.160.148'})
+
+window.location.href.indexOf('127.0.0.1') === -1 && Posthog.init("[iHGijN_zMe0ULDD9khFpPOcOfUGpwxl4nokVNsMvHEY]", {api_host: 'http://159.203.160.148'}) 
 
 new Vue({
   render: h => h(App),
